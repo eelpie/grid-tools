9ba14d4915a201bc72ca0dde08fa619c02b3c627
Tony McCrae <tony@eelpieconsulting.co.uk>
2025-01-29 22:10:44 +0000

    set profile

--- ingest/awscli-put.bash
+++ ingest/awscli-put.bash
@@ -1,4 +1,4 @@
 IMAGE=IMG_2842.JPG
 GRID=fip-test-1
 
-aws --endpoint-url http://storage.googleapis.com s3 cp $IMAGE s3://hostedgrid-ingest/$GRID/feeds/$IMAGE
+aws --profile=hostedgrid-ingest --endpoint-url http://storage.googleapis.com s3 cp $IMAGE s3://hostedgrid-ingest/$GRID/feeds/$IMAGE
