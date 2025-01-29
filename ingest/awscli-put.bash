454b878725d15abdb9a920833135083f84815467
Tony McCrae <tony@eelpieconsulting.co.uk>
2025-01-29 23:04:56 +0000

    Move faster setup with empty region from profile to cmd

--- ingest/awscli-put.bash
+++ ingest/awscli-put.bash
@@ -1,4 +1,4 @@
 IMAGE=IMG_2842.JPG
 GRID=fip-test-1
 
-aws --profile=hostedgrid-ingest --endpoint-url http://storage.googleapis.com s3 cp $IMAGE s3://hostedgrid-ingest/$GRID/feeds/$IMAGE
+aws --profile=hostedgrid-ingest --region= --endpoint-url http://storage.googleapis.com s3 cp $IMAGE s3://hostedgrid-ingest/$GRID/feeds/$IMAGE
